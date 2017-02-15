142cc1837179fa515c2751ec7acc479a8eac194a
FREAL-SAISON <s.frealsaison@gmail.com>
2017-02-15 23:40:13 +0100

    Adaptation du script pour traitement auto des fichiers d'un repertoire

--- import_data_-_parsing_json_to_sqlite.py
+++ import_data_-_parsing_json_to_sqlite.py
@@ -1,35 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Chargement des données Velib du site JCDecaux (https://developer.jcdecaux.com/)
+Chargement des données Velib du site http://vlsstats.ifsttar.fr/rawdata/
 Les données traitées correspondent aux fichiers .gz produits à une fréquence
 mensuelle.
 Il est nécessaire de télécharger les données pour les traiter avec ce script.
-Ce script ne traite pas les données accessibles via l'API JCD.
+Ce script ne traite pas les données accessibles via l'API JCD temps réel.
 """
 
 import json
 import gzip
 import pandas as pd
 import sqlalchemy as sa
+import os
 
-disk_engine = sa.create_engine('sqlite:///data/SQLiteData/Velib_raw_Data.sqlite')
-
-jsonfilename = 'data/data_all_Paris/data_all_Paris.jjson_2017-01-01-1483248351.gz'
-#jsonfilename = 'data/data_all_Paris/data_all_Paris.jjson_2016-12-01-1480569957.gz'
-#jsonfilename = 'data/data_all_Paris/data_all_Paris.jjson_2015-02-12-1423718749.gz'
 
 def gzip_Json_to_str(jsonfilename):
     with gzip.GzipFile(jsonfilename, 'r') as file:        
         json_bytes = file.read()         
-        json_str = json_bytes.decode('utf-8')            
+        json_str = json_bytes.decode('utf-8')#, errors='ignore')            
         data = json.loads(json.dumps(json_str))[1:-2]
         return(data) 
+
 
 def gzip_Json_todf(jsonfilename):
     data = gzip_Json_to_str(jsonfilename)
     data1 = data.replace(']',',\n')
     data2 =  "[" + data1.replace('[', '') + "]"
     return(pd.read_json(data2, orient='records'))
+
 
 def gzip_Json_to_sqlite(jsonfilename):
     data_frame = gzip_Json_todf(jsonfilename)
@@ -40,9 +38,16 @@ def gzip_Json_to_sqlite(jsonfilename):
         if c not in columns:
             data_frame = data_frame.drop(c, axis=1) 
 
-    #engine = create_engine('mysql+mysqldb://scott:tiger@localhost/foo')
+    #TODO : engine = create_engine('mysql+mysqldb://meee:pass@localhost/foo')
     data_frame.to_sql('data', disk_engine, chunksize=1000, if_exists='append')
-    # or absolute, starting with a slash:
-    #engine = create_engine('sqlite:////absolute/path/to/foo.db')
-
-gzip_Json_to_sqlite(jsonfilename)
+    # alternative : absolute path starting with an additional slash
+   
+if __name__ == "__main__":
+    disk_engine = sa.create_engine('sqlite:///data/SQLiteData/Velib_raw_Data.sqlite')
+    
+    for file in os.listdir('data/data_all_Paris/'):
+        file = 'data/data_all_Paris/' + file
+        try :
+            gzip_Json_to_sqlite(file)
+        except :
+            print('Error while parsing : ' + file)
